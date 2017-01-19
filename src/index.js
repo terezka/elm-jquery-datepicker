72667fb6cfc3b4b6cc453cd06c5182054e80ec18
Tereza Sokol <terezasokol@gmail.com>
2017-01-19 23:26:35 +0100

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 require('./main.css');
 
+// Get Elm code
 var Elm = require('./Main.elm');
 
+// Find mount element
 var root = document.getElementById('root');
 
+// Mount Elm app
 var app = Elm.Main.embed(root);
 
+
+// Initilize when Elm is ready
 app.ports.initializeJquery.subscribe(function() {
-    $( "#datepicker" )
-      .datepicker({
-        onSelect: function(selectedDate) {
-          app.ports.newDate.send(selectedDate);
-        }
-      });
+    $("#datepicker").datepicker({
+      onSelect: function(selectedDate) {
+        // Send date to Elm when new date is picked
+        app.ports.newDate.send(selectedDate);
+      }
+    });
 });
